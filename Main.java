3800a2a88259a525bfa27938f8f75711001310a2
valeri1383 <valeri1383@gmail.com>
2023-10-24 22:11:41 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,53 @@
+import java.io.Console;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+
+        /*
+        1.	Write a Java program to find the largest and smallest numbers in an array.
+        int[] myNum = {10, 20, 3, 69, 30, 40};
+        int biggest = -99999999;
+        int smallest = 99999999;
+        int n = myNum.length;
+
+        for (int i = 0; i < n; i++) {
+            if (myNum[i] > biggest){
+                biggest = myNum[i];
+            }
+            if (myNum[i] < smallest){
+                smallest = myNum[i];
+            }
+        }
+
+        //System.out.println("Biggest - " + biggest);
+        //System.out.println("Smallest - " + smallest);
+        */
+
+        /*
+        2.	Write a Java program to calculate the area of a circle.
+        Scanner scanner = new Scanner(System.in);
+        //System.out.print("Insert the radius: ");
+        float radius = scanner.nextFloat();
+
+        double area_result = radius * radius * Math.PI;
+        //System.out.println("Circle area: " + area_result);
+        scanner.close();
+        */
+
+        /*
+        6.	Write a Java program to reverse a string.
+
+        Scanner scanner_1 = new Scanner(System.in);
+        System.out.print("Please insert text: ");
+        String text = scanner_1.nextLine();
+        StringBuilder reversed_text = new StringBuilder();
+        reversed_text.append(text);
+        reversed_text.reverse();
+        System.out.println(reversed_text);
+        */
+
+
+    }
+}
